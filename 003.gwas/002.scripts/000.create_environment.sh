2402e6c7c10f5047fd3662c7e0661d38661dbcdb
picla <pieter.clauw@gmi.oeaw.ac.at>
2022-12-13 12:57:49 +0100

    conda environment in general folder

--- 003.gwas/002.scripts/000.create_environment.sh
+++ 003.gwas/002.scripts/000.create_environment.sh
@@ -4,4 +4,4 @@
 # in case conda is not yet available, check link below for installation guides
 # https://conda.io/projects/conda/en/latest/user-guide/install/index.html
 
-conda env create -f 001.data/environment.yml
+conda env create -f environment.yml

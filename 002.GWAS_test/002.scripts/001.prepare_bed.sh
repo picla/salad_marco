f4694dd575356071c68a15c0ef0231d0e6e878a6
picla <pieter.clauw@gmi.oeaw.ac.at>
2022-12-13 12:52:53 +0100

    add comments

--- 002.GWAS_test/002.scripts/001.prepare_bed.sh
+++ 002.GWAS_test/002.scripts/001.prepare_bed.sh
@@ -10,24 +10,33 @@ ml plink/2.00-alpha1-20190826-avx
 ml bcftools/1.9-foss-2018b
 
 # DATA #
+## general data directory, containing the Lactuca VCF file, phenotypes and ID list
 DATAdir=/groups/nordborg/user/pieter.clauw/Documents/Collaborations/salad_marco/000.generalData/001.data/
+## general working directory
 SCRATCHdir=/scratch-cbe/users/pieter.clauw/salad_marco/002.GWAS_test/
+## directory for temporary files
 TEMP=/scratch-cbe/users/pieter.clauw/temp/
 
+## The general Lactuca VCF file (https://ftp.cngb.org/pub/CNSA/data2/CNP0000335/Other/variation/Lactuca.snp.vcf.gz)
 VCF=${DATAdir}Lactuca.snp.vcf.gz
+## phenotypes with first column containing the TKI ID for each accession, subsequent column(s) contain phenotype measurements
 PHENO=${DATAdir}tableS10_agronomicTraits.txt
+## annotation file for running IDs and TKI IDs
 IDkey=${DATAdir}445LettuceRunningIDs.txt
 
-ID_MAP=${TEMP}ID_map.txt
+## temporary file storing VCF sample names
 VCF_IDs=${TEMP}VCF_IDs.txt
+## temporary file to link VCF sample names to TKI IDs
+ID_MAP=${TEMP}ID_map.txt
+## temporary file with the list of phenotyped accessions
 IDsub=${TEMP}IDsub.txt
-
+## subset of VCF file containing only phenotyped accessions
 VCFsub=${SCRATCHdir}001.data/$(basename -s .vcf.gz $VCF).TIK.sub.vcf.gz
 
 # filter VCF for phenotyped accessions
 ## get VCF sample names
 bcftools query -l $VCF > $VCF_IDs
-## link VCF sample names to TIK IDs
+## link VCF sample names to TKI IDs
 awk 'NR==FNR{ a[$1]=$2; next }$1 in a{ $2=a[$1]; print }' $IDkey $VCF_IDs > $ID_MAP
 # get phenotyped accessions
 awk '(NR > 1) {print $1}' $PHENO > $IDsub
@@ -35,7 +44,7 @@ awk '(NR > 1) {print $1}' $PHENO > $IDsub
 bcftools reheader -s $ID_MAP $VCF | bcftools view -a -S $IDsub -Oz -o $VCFsub
 tabix $VCFsub
 
-# create .bed .fam and .bim files
+# create .bed .fam and .bim files, required for GWAS.
 plink2 --vcf $VCFsub --make-bed --out ${VCFsub/.vcf.gz/}
 
 # cleanup
--- 002.GWAS_test/002.scripts/002.GWAS.sh
+++ 002.GWAS_test/002.scripts/002.GWAS.sh
@@ -6,6 +6,7 @@
 #SBATCH --output=999.logs/002.GWAS_%A_%a.log
 #SBATCH --array=3-10
 
+# load required software
 ml build-env/2020
 ml plink/2.00-alpha1-20190826-avx
 
@@ -13,50 +14,64 @@ ml plink/2.00-alpha1-20190826-avx
 source activate /users/pieter.clauw/.conda/envs/gemma
 
 # DATA #
+## this variable will select the phenotype column for GWAS
 i=$SLURM_ARRAY_TASK_ID
 
+## general data directory, containing the phenotype data
 DATAdir=/groups/nordborg/user/pieter.clauw/Documents/Collaborations/salad_marco/000.generalData/001.data/
+## directory for storing GWAS results
 RESULTSdir=/groups/nordborg/user/pieter.clauw/Documents/Collaborations/salad_marco/002.GWAS_test/003.results/
+## working directory
 SCRATCHdir=/scratch-cbe/users/pieter.clauw/salad_marco/002.GWAS_test/
 
+## phenotypes with first column containing the TKI ID for each accession, subsequent column(s) contain phenotype measurements
 PHENO=${DATAdir}tableS10_agronomicTraits.txt
+## bed file containing genotype information, created with 001.prepare_bed.sh
 BED=${SCRATCHdir}001.data/Lactuca.snp.TIK.sub
+## directory for temporary files
 TEMP=/scratch-cbe/users/pieter.clauw/temp/
 
+
+## extract the name of the phenotype (column name)
 PHENOname=$(awk -v i=$i 'BEGIN {FS = "\t"}(NR == 1){print $i}' $PHENO | sed "s/ /_/g")
+## temporary file storing the phenptype data
 PHENOtemp=${TEMP}${PHENOname}.txt
 
+## temporary file combining genotype and phenotype information. Input for GWAS
 GWASinput=${TEMP}$(basename $BED).${PHENOname}
+## file storing the kinship matrix, will be saved in GEMMAdir
 K=kinship_${PHENOname}
+## directory for running GWAS and storing output
 GEMMAdir=${SCRATCHdir}003.results/GEMMA/
 
+## output whihc phenotype is being used
 echo job $i corresponds to phenotype $PHENOname
 
-# select phenotype
+# select phenotype and store in temporary file
 awk -v i=$i '(NR > 1){print 0, $1, $i}' $PHENO > $PHENOtemp
+## remove NA values
 sed -i 's/ -/ NA/g' $PHENOtemp
 
 # update .fam file, prepare .bed file for GWAS
 plink2 --bfile $BED --pheno $PHENOtemp --make-bed --out $GWASinput
 
-# PCA for covariates
+# run PCA that will be used as covariates for population structure correction (in addition to kinship)
 plink2 --bfile $GWASinput --pca 5 --out $GWASinput
-
 awk '(NR > 1) {print 1, $3, $4, $5, $6, $7}' ${GWASinput}.eigenvec > ${GWASinput}.covariates
 
 # change to GEMMAdir, where output will be written
 mkdir -p $GEMMAdir
 cd $GEMMAdir
 
-# kinship
+# calculate kinship and save in K
 gemma -bfile $GWASinput -gk 1 -o $K
 
-# GWAS
+# run GWAS
 gemma -bfile $GWASinput -k output/${K}.cXX.txt -c ${GWASinput}.covariates -lmm 4 -maf 0.05 -o $PHENOname
 
 # cleanup
 rm -v ${GWASinput}*
 rm -v $PHENOtemp
 
-# copy to projects
+# copy to results to the RESULTSdir
 cp ${GEMMAdir}output/${PHENOname}* $RESULTSdir
